6d89f65a5533acdb92f51e8941db82c146083417
Andreas Theodosiou <andreasabu@gmail.com>
2010-07-29 21:00:34 +0300

    Interactive mode is finally complete. Problems were faced with assigning the entity dictionary, due to the fact that assigning a name to a list in python does not create a new copy of the list but refers to the same memory location, thus the [:] operator had to be used to create a copy of the list. Added support for about, help, and quit commands. Everything is tested and seems to be working as expected. This should merge with the master branch pretty soon.

--- unity.py
+++ unity.py
@@ -47,29 +47,51 @@ def get_unit(dic):
 def interactive_get_data(dic):
     while True:
         try:
+            list = []
             line = input('Enter the your quantity and the unit: ')
             data_list = str.split(line)
             if not data_list:
                 print('That is useless')
                 continue
+            elif str(data_list[0]) == 'help':
+                print()
+                print('''Hello unity is a simple humble unit convert. Input a number followed by a unit, and unity will try to output a list
+                possible unit conversions''')
+                print()
+                continue
+            elif str(data_list[0]) == 'about':
+                print()
+                print('Unity Version 0.2')
+                print('Author: Andreas Theodosiou')
+                print()
+                continue
+            elif str(data_list[0]) == 'quit':
+                print()
+                print('Unity is going to bed')
+                print('Goodbye')
+                print()
+                sys.exit()
             else:
                 quantity_test = float(data_list[0])
                 if quantity_test >= 1:
                     quantity = quantity_test
-                    return quantity
+                    list.append(quantity)
                 else:
                     print('You have to enter a number that is greater than zero')
                     continue
                 unit_test = str(data_list[1])
                 if unit_test in dic:
                     unit = unit_test
-                    return unit
+                    list.append(unit)
                 else:
                     print('The unit', unit_test, 'is not supported by unity')
                     continue
+                if list:
+                    return list
         except ValueError as error:
             print('Input [quantity] [unit]')
             print(error)
+
 
 def get_entity(unit, dic):
     entity = dic[unit][0]
@@ -103,37 +125,38 @@ def print_conversion(conv_list, base_unit):
 
 
 if len(sys.argv) < 2:
-    print('No action specified')
-    print('Terminating unity')
-    sys.exit()
+    entities = {'speed': [['m/s', 1], ['km/h', 3.6], ['mph', 2.237], ['kn', 1.944]], 'length': [['m', 1], ['ft', 3.281]]}
+    print('No command line arguments found, entering interactive mode')
+    while True:
+        original_quantity, original_unit = interactive_get_data(units)
+        entity = get_entity(original_unit, units)
+        base_unit = convert_to_base(original_quantity, original_unit, units)
+        conversion_list = entities[entity][:]
+        formatted_conv_list = format_conv_list(conversion_list, original_unit)
 
-original_quantity = get_quantity()
-original_unit = get_unit(units)
-entity = get_entity(original_unit, units)
-base_unit = convert_to_base(original_quantity, original_unit, units)
-conversion_list = entities[entity]
+        print()
+        print('Input:', original_quantity, original_unit)
+        print('Interpretation', entity)
+        print('Unit conversions:')
 
-formatted_conv_list = format_conv_list(conversion_list, original_unit)
-#index = 0
-#while index < len(conversion_list):
-#    if original_unit in conversion_list[index]:
-#        del conversion_list[index]
-#        formatted_conversion_list = conversion_list
-#        break
-#    else:
-#        index += 1
-#        continue
+        print_conversion(formatted_conv_list, base_unit)
+        print()
 
-print('Input:', original_quantity, original_unit)
-print('Interpretation', entity)
-print('Unit conversions:')
 
-print_conversion(formatted_conv_list, base_unit)
-#idx = 0 
-#while idx < len(formatted_conversion_list):
-#    unit = str(formatted_conversion_list[idx][0])
-#    rate = float(formatted_conversion_list[idx][1])
-#    converted_quantity = base_unit * rate
-#    print(converted_quantity, unit)
-#    idx += 1
+
+else:
+    original_quantity = get_quantity()
+    original_unit = get_unit(units)
+    entity = get_entity(original_unit, units)
+    base_unit = convert_to_base(original_quantity, original_unit, units)
+    conversion_list = entities[entity]
+
+    formatted_conv_list = format_conv_list(conversion_list, original_unit)
+
+
+    print('Input:', original_quantity, original_unit)
+    print('Interpretation', entity)
+    print('Unit conversions:')
+
+    print_conversion(formatted_conv_list, base_unit)
 
