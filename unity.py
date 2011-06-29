5bdd35a6c7126e22a508afa890ebaf93d2a1ef40
Andreas Theodosiou <andreasabu@gmail.com>
2011-06-29 10:33:47 +0300

    Major changes. The entities dictionaey has been completely removed. Only the units dictionary is used now. The decimal library is used to process number instead of float. This will lead to more accurate results. Have also added support for "unity.py about", unity will now display its version.

--- unity.py
+++ unity.py
@@ -16,32 +16,34 @@
 
 # Filename: unity.py
 # Author: Andreas Theodosiou
-# Description: A tool that accepts a quantity in a certain unit, and outputs
-# that quantity in a number of other units.
+# Description: A tool that accepts a quantity in a certain unit, and outputs that quantity in a number of other units.
 
-import sys, string
+import sys, string, decimal
 
 # Units will store the units supported by unity, entity in which they belong
 # and the conversion rate which will be used to convert them to the base unit
 # for that entity.
 
-units = {'m/s': ('speed', 1.0), 'km/h': ('speed', 0.2778), 'mph': ('speed', 0.447), 'kn': ('speed', 0.5144), 'm': ('length', 1), 'ft': ('length', 0.3048), 'in': ('length', 0.0254), 'cm': ('length', 0.01), 'mm': ('length', 0.001)} 
+units = {'m/s': ('speed', 1.0), 'km/h': ('speed', 0.2777777777777778), 'mph': ('speed', 0.44704), 'kn': ('speed', 0.5144), 'm': ('length', 1), 'ft': ('length', 0.3048), 'in': ('length', 0.0254), 'cm': ('length', 0.01), 'mm': ('length', 0.001)} 
 
-# Entities will store all the units and conversion rates for those units of
-# each entity.
+# Generate the units dictionary using the Decimal data type instead of float.
+units = {keys:(value1, decimal.Decimal(value2)) for keys, (value1, value2) in units.items()}
 
-entities = {'speed': (('m/s', 1), ('km/h', 3.6), ('mph', 2.237), ('kn', 1.944)), 'length': (('m', 1), ('ft', 3.281), ('in', 39.37), ('cm', 100), ('mm', 1000))}
-
-# get_quantity() will attempt to get the first argument from the command line, if there is any and convert it to a float. c
+# get_quantity() will attempt to get the first argument from the command line, if there is any and convert it to a Decimal.
 def get_quantity():
     try:
-        quantity_test = float(sys.argv[1])
+        quantity_test = decimal.Decimal(sys.argv[1])
         quantity = quantity_test
         return quantity
-    except ValueError:
-        print('Usage unity.py [quantity] [unit]')
-        print('In other words you are using unity wrong')
-        sys.exit()
+    except:
+        quantity_test = sys.argv[1]
+        if quantity_test == "about":
+            print("Unity V0.5")
+            sys.exit()
+        else:
+            print('Usage unity.py [quantity] [unit]')
+            print('In other words you are using unity wrong')
+            sys.exit()
 
 # get_unit(dic) gets the unit from the command line parameter, checks if it exists inside the dictionary dic, if it does it returns the unit as a string. If it does not
 # informs the user that the unit used is not supported 
@@ -81,7 +83,7 @@ def interactive_get_data(dic):
                 continue
             elif str(data_list[0]) == 'about':
                 print()
-                print('Unity Version 0.2')
+                print('Unity Version 0.5')
                 print('Author: Andreas Theodosiou')
                 print('Please report bugs at http://github.com/MajorBiscuit/Unity/issues')
                 print()
@@ -93,7 +95,7 @@ def interactive_get_data(dic):
                 print()
                 sys.exit()
             else:
-                quantity_test = float(data_list[0])
+                quantity_test = decimal.Decimal(data_list[0])
                 quantity = quantity_test
                 list.append(quantity)
                 unit_test = str(data_list[1])
@@ -122,50 +124,39 @@ def get_entity(unit, dic):
     entity = dic[unit][0]
     return entity
 
-# convert_to_base(quantity, unit, dic) finds the entry for the unit received by the user inside the dictionary and uses its conversion to base unit rate to convert it to a base unit
+# convert_to_base(quantity, unit, dic) finds the entry for the unit received by the user inside the dictionary and uses its conversion to base unit rate to convert it to a base unit.
 def convert_to_base(quantity, unit, dic):
     conversion_rate = dic[unit][1]
     base_unit = quantity * conversion_rate
     return base_unit
 
-# This function goes through the tuple of units and rates and removes the entry of the original quantity's unit in order to avoid converting the base quantity back to the original quantity entered by the user.
-def format_conv_tuple(conv_tuple, unit):
-    index = 0
-    while index < len(conv_tuple):
-        if unit in conv_tuple[index]:
-            formatted_conv_tuple = conv_tuple[:index]+conv_tuple[index+1:]
-            return formatted_conv_tuple
-            break
-        else:
-            index += 1
-            continue
+# generate_unit_conversion_list() generates the conversion list using the units dictionary. It removes the unit used by the user from the list and the units which do not belong to the same entity as the unit entered by the user.
+def generate_unit_conversion_list():
+    units_list = list(units.keys())
+    units_list.remove(original_unit)
+    return [unit for unit in units_list if units[unit][0] == entity]
 
-# print_conversion(conv_tuple, base_unit) iterates through the conversion tuple and mutliplies the base_unit with the conversion rates to find the corresponding quantities in different units.
-def print_conversion(conv_tuple, base_unit):
-    index = 0
-    while index < len(conv_tuple):
-        unit = str(conv_tuple[index][0])
-        rate = float(conv_tuple[index][1])
-        converted_quantity = base_unit * rate
-        print(converted_quantity, unit)
-        index += 1
-
+# run_conversion(base_quantity, unit_conversion_list)) converts the original quantity into the appropriate units.
+def run_conversion(base_quantity, unit_conversion_list):
+	for unit in unit_conversion_list:
+		converted_quantity = round(base_quantity * (1/units[unit][1]), 3)
+		print(converted_quantity, unit)
+		
 # Check if there are arguments passed when running the apllication.
 if len(sys.argv) < 2: #No arguments
     print('No command line arguments found, entering interactive mode')
     while True:
         original_quantity, original_unit = interactive_get_data(units) # Get input from the user
         entity = get_entity(original_unit, units)  # Find the type of quantity of the input
-        base_unit = convert_to_base(original_quantity, original_unit, units) # Convert to the base unit for that quantity
-        conversion_tuple = entities[entity][:] # Get tuple of units for that type of quantity
-        formatted_conv_tuple = format_conv_tuple(conversion_tuple, original_unit) # Remove input quantity from the tuple
+        base_quantity = convert_to_base(original_quantity, original_unit, units) # Convert to the base unit for that quantity
+        unit_conversion_list = generate_unit_conversion_list() # Generate the list of appropriate units for the converion at hand.
 
         print()
         print('Input:', original_quantity, original_unit)
         print('Interpretation', entity)
         print('Unit conversions:')
 
-        print_conversion(formatted_conv_tuple, base_unit) # Run the conversion proccess and print the results
+        run_conversion(base_quantity, unit_conversion_list) # Run the conversion proccess and print the results
         print()
 
 # In case there were arguments, get quantity and unit from command line arguments and repeat the process of converting.
@@ -173,9 +164,8 @@ else:
     original_quantity = get_quantity()
     original_unit = get_unit(units)
     entity = get_entity(original_unit, units)
-    base_unit = convert_to_base(original_quantity, original_unit, units)
-    conversion_tuple = entities[entity]
-    formatted_conv_tuple = format_conv_tuple(conversion_tuple, original_unit)
+    base_quantity = convert_to_base(original_quantity, original_unit, units)
+    unit_conversion_list = generate_unit_conversion_list()
 
 
     print()
@@ -183,5 +173,5 @@ else:
     print('Interpretation', entity)
     print('Unit conversions:')
 
-    print_conversion(formatted_conv_tuple, base_unit)
+    run_conversion(base_quantity, unit_conversion_list)
 
